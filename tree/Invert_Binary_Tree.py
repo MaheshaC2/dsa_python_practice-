99a45230796f0fbb262cc075144613d8f23a112c
MaheshaC2 <maheshac6475@gmail.com>
2026-04-25 21:51:44 +0530

    Implement binary tree inversion function

---
+++ tree/Invert_Binary_Tree.py
@@ -0,0 +1,12 @@
+class Node:
+    def _init_(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def invert(root):
+    if not root:
+        return None
+
+    root.left, root.right = invert(root.right), invert(root.left)
+    return root
