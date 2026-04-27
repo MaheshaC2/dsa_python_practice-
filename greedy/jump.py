256248687ad771cede710053a451a1b0a5411e2a
MaheshaC2 <maheshac6475@gmail.com>
2026-04-27 21:08:18 +0530

    Implement jump game solution in jump.py

---
+++ greedy/jump.py
@@ -0,0 +1,11 @@
+nums = [2,3,1,1,4]
+
+max_reach = 0
+
+for i in range(len(nums)):
+    if i > max_reach:
+        print(False)
+        break
+    max_reach = max(max_reach, i + nums[i])
+else:
+    print(True)
