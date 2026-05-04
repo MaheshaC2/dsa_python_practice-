eede8c3af343a3c0fb6d100be58304f8ee07dba3
MaheshaC2 <maheshac6475@gmail.com>
2026-05-04 22:58:21 +0530

    Add Fibonacci function with dynamic programming
    Implement Fibonacci function using dynamic programming.

---
+++ dynamic_programming/fibo.py
@@ -0,0 +1,16 @@
+# Problem: Fibonacci using DP
+# Category: Dynamic Programming
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+def fib(n):
+    if n <= 1:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
