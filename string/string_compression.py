68e68d950b4901be518ea47b4c3093c92205454e
MaheshaC2 <maheshac6475@gmail.com>
2026-05-06 22:35:04 +0530

    Add string compression function
    Implement string compression function to reduce repeated characters.

---
+++ string/string_compression.py
@@ -0,0 +1,24 @@
+"""
+Problem: String Compression
+Category: Strings
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def compress(chars):
+    i = 0
+    res = []
+
+    while i < len(chars):
+        char = chars[i]
+        count = 0
+
+        while i < len(chars) and chars[i] == char:
+            i += 1
+            count += 1
+
+        res.append(char)
+        if count > 1:
+            res.append(str(count))
+
+    return "".join(res)
