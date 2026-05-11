020c85088a920be50a3e1a6fc0f2ed3c28131465
MaheshaC2 <maheshac6475@gmail.com>
2026-05-11 22:09:43 +0530

    Add recursive factorial function in factorial.py
    Implement recursive function to calculate factorial.

---
+++ Recursion/factorial.py
@@ -0,0 +1,8 @@
+# Problem: Factorial Using Recursion
+# Category: Recursion
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
