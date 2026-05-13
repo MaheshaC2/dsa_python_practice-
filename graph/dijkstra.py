c881e167e3f340a9966ecb18cfb608b136d441fc
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-05-13 22:57:01 +0530

    Create dijkstra.py

---
+++ graph/dijkstra.py
@@ -0,0 +1,26 @@
+# Problem: Dijkstra's Shortest Path
+# Category: Graphs
+# Time Complexity: O(E log V)
+# Space Complexity: O(V)
+
+import heapq
+
+def dijkstra(graph, start):
+    pq = [(0, start)]
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+
+        if current_distance > distances[current_node]:
+            continue
+
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
