4158a6c189f05eb9cf164e00d6f0f1265c293a37
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-05-19 23:15:40 +0530

    Create path_sum.py

---
+++ tree/path_sum.py
@@ -0,0 +1,21 @@
+"""
+Problem: Path Sum
+Category: Trees
+Time Complexity: O(n)
+Space Complexity: O(h)
+"""
+
+def has_path_sum(root, target_sum):
+
+    if not root:
+        return False
+
+    if not root.left and not root.right:
+        return target_sum == root.val
+
+    target_sum -= root.val
+
+    return (
+        has_path_sum(root.left, target_sum) or
+        has_path_sum(root.right, target_sum)
+    )
