e929ce1d3b1f5efdc39da63cc96eea262a8f6ce6
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-05-24 23:07:05 +0530

    Add max_product function for maximum product subarray
    Implement max_product function to find the maximum product of a subarray.

---
+++ array/max.py
@@ -0,0 +1,21 @@
+"""
+Problem: Maximum Product Subarray
+Category: Arrays
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def max_product(nums):
+
+    current_max = current_min = result = nums[0]
+
+    for num in nums[1:]:
+
+        temp = current_max * num
+
+        current_max = max(num, temp, current_min * num)
+        current_min = min(num, temp, current_min * num)
+
+        result = max(result, current_max)
+
+    return result
