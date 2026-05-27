4e8dc647ca582e8a0a977a2c498a02207393572e
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-05-27 23:28:07 +0530

    Implement max_depth function for binary tree

---
+++ tree/max_depth.py
@@ -0,0 +1,13 @@
+"""
+Problem: Maximum Depth of Binary Tree
+Category: Trees
+Time Complexity: O(n)
+Space Complexity: O(h)
+"""
+
+def max_depth(root):
+
+    if not root:
+        return 0
+
+    return 1 + max(max_depth(root.left), max_depth(root.right))
