b0ff1702bd76174b157d6e797802582eb3af1595
MaheshaC2 <256048071+MaheshaC2@users.noreply.github.com>
2026-05-29 21:50:30 +0530

    Create end.py

---
+++ linked_list/end.py
@@ -0,0 +1,24 @@
+"""
+Problem: Remove Nth Node From End of List
+Category: Linked List
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def remove_nth_from_end(head, n):
+
+    dummy = ListNode(0)
+    dummy.next = head
+
+    slow = fast = dummy
+
+    for _ in range(n + 1):
+        fast = fast.next
+
+    while fast:
+        slow = slow.next
+        fast = fast.next
+
+    slow.next = slow.next.next
+
+    return dummy.next
